3a8fa1b5c03e40cc90afa648d852657e473f37cb
Jan Tojnar <jtojnar@gmail.com>
2022-07-23 15:44:47 +0000

    evolution: 3.44.3 → 3.45.1
    https://gitlab.gnome.org/GNOME/evolution/-/compare/3.44.3...3.45.1

--- pkgs/applications/networking/mailreaders/evolution/evolution/default.nix
+++ pkgs/applications/networking/mailreaders/evolution/evolution/default.nix
@@ -46,11 +46,11 @@
 
 stdenv.mkDerivation rec {
   pname = "evolution";
-  version = "3.44.3";
+  version = "3.45.1";
 
   src = fetchurl {
     url = "mirror://gnome/sources/evolution/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "U2sR9BM99vIW8nr5okgaGe164Ivi1KE7EoBhwmKjZJk=";
+    sha256 = "/iRZq5DdZ86JdtWuu6ekP69gqvBJMeTWnxxG10ensW4=";
   };
 
   nativeBuildInputs = [
